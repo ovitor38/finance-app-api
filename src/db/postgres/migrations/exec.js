b74fcf2d295436967f39ed7b1e55ba75ca196a33
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-02-25 23:42:51 -0300

    feat(db): add init migration to create users and transactions

---
+++ src/db/postgres/migrations/exec.js
@@ -0,0 +1,23 @@
+import fs from "fs";
+import { pool } from "../helper.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const execMigrationsasync = async () => {
+    const client = await pool.connect();
+
+    try {
+        const filePath = path.join(__dirname, "01-init.sql");
+        const script = fs.readFileSync(filePath, "utf-8");
+        await client.query(script);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        await client.release;
+    }
+};
+
+execMigrationsasync();
