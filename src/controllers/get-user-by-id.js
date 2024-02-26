d897184e9fce5e957dd7a9f774446ed2a4b21351
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-02-26 16:37:05 -0300

    feat(controller): ensure a message if user not found

--- src/controllers/get-user-by-id.js
+++ src/controllers/get-user-by-id.js
@@ -1,6 +1,6 @@
 import validator from "validator";
 import { GetUserByIdUseCase } from "../use-cases/get-user-by-id.js";
-import { badRequest, ok, serverError } from "./helpers.js";
+import { badRequest, notFound, ok, serverError } from "./helpers.js";
 
 export class GetUserByIdController {
     async execute(httpRequest) {
@@ -13,6 +13,10 @@ export class GetUserByIdController {
             const user = await getUserByIdUseCase.execute(
                 httpRequest.params.userId,
             );
+
+            if (!user) {
+                return notFound({ message: "User not found" });
+            }
 
             return ok(user);
         } catch (error) {
