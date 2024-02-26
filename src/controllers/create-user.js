edf35c512534cbc5dc4320627db39c1ce5e789c5
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-02-26 14:33:56 -0300

    feat(controller): add helper for controller

--- src/controllers/create-user.js
+++ src/controllers/create-user.js
@@ -1,12 +1,12 @@
 import validator from "validator";
 import { CreateUserUseCase } from "../use-cases/create-users.js";
+import { badRequest, created } from "./helpers.js";
 
 export class CreateUserController {
     async execute(httpRequest) {
         try {
             const params = httpRequest.body;
 
-            // validar a requisição (campos obrigatórios e tamanho de senha e email)
             const requiredFields = [
                 "first_name",
                 "last_name",
@@ -16,43 +16,24 @@ export class CreateUserController {
 
             for (const field of requiredFields) {
                 if (!params[field] || params[field].trim().length === 0) {
-                    return {
-                        statusCode: 400,
-                        body: {
-                            errorMessage: `Missing param ${field}`,
-                        },
-                    };
+                    badRequest({ message: `Missing param ${field}` });
                 }
             }
 
             if (params.password.length < 6) {
-                return {
-                    statusCode: 400,
-                    body: {
-                        errorMessage: "Password must be least 6 chacteres",
-                    },
-                };
+                badRequest({ message: "Password must be least 6 chacteres" });
             }
 
             const emailIsValid = validator.isEmail(params.email);
 
             if (!emailIsValid) {
-                return {
-                    statusCode: 400,
-                    body: {
-                        errorMessage: "The email is invalid",
-                    },
-                };
+                badRequest({ message: "The email is invalid" });
             }
 
-            // chamar use case
             const createUserUseCase = new CreateUserUseCase();
             const createdUser = await createUserUseCase.execute(params);
 
-            return {
-                statusCode: 201,
-                body: createdUser,
-            };
+            return created(createdUser);
         } catch (error) {
             console.log(error);
             return {
@@ -62,6 +43,5 @@ export class CreateUserController {
                 },
             };
         }
-        // retornar a resposta para o usuário(status code)
     }
 }
