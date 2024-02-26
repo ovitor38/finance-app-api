7b94d75f848e067454ea546798dd412dd7697c7b
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-02-25 22:56:14 -0300

    chore(db): add pg and dotenv setup

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const names = "Victor Oss.";
-console.log(names);
+import "dotenv/config.js";
+import { PostgresHelper } from "./src/db/postgres/helper.js";
+import express from "express";
+
+const app = express();
+
+app.get("/", async (req, res) => {
+    const results = await PostgresHelper.query("SELECT * FROM users;");
+    res.send(JSON.stringify(results));
+});
+
+app.listen(3000, () => {
+    console.log("hello server running on port 3000");
+});
