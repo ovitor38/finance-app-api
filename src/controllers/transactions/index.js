b326ee1de49fa3c7919a87cba1942dae1c966984
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-03-05 11:34:23 -0300

    feat(controller): add update transaction controller

--- src/controllers/transactions/index.js
+++ src/controllers/transactions/index.js
@@ -1,2 +1,3 @@
 export * from "./create-transaction.js";
 export * from "./get-transactions-by-user-id.js";
+export * from "./update-transaction.js";
---
+++ src/controllers/transactions/update-transaction.js
@@ -0,0 +1,65 @@
+import {
+    badRequest,
+    checkIfAmountIsValid,
+    checkIfIdIsValid,
+    checkIfTypeIsValid,
+    invalidAmountResponse,
+    invalidIdResponse,
+    invalidTypeResponse,
+    ok,
+    serverError,
+} from "../helpers/index.js";
+
+export class updateTransanctionController {
+    constructor(updateTransanctionUseCase) {
+        this.updateTransanctionUseCase = updateTransanctionUseCase;
+    }
+
+    async execute(httpRequest) {
+        try {
+            const idValid = checkIfIdIsValid(httpRequest.params.transactionId);
+
+            if (!idValid) {
+                return invalidIdResponse();
+            }
+
+            const params = httpRequest.body;
+
+            const allowedFields = ["name", "date", "amount", "type"];
+
+            const someFieldsIsNotAllowed = Object.keys(params).some(
+                (field) => !allowedFields.includes(field),
+            );
+
+            if (!someFieldsIsNotAllowed) {
+                return badRequest({ message: "Some fields is not allowed" });
+            }
+
+            if (params.amount) {
+                const amountIsValid = checkIfAmountIsValid(params.amount);
+
+                if (!amountIsValid) {
+                    return invalidAmountResponse();
+                }
+            }
+
+            if (params.type) {
+                const typeIsValid = checkIfTypeIsValid(params.type);
+
+                if (!typeIsValid) {
+                    return invalidTypeResponse();
+                }
+            }
+
+            const transaction = await this.updateTransanctionUseCase.execute(
+                httpRequest.body.transactionId,
+                params,
+            );
+
+            return ok(transaction);
+        } catch (error) {
+            console.log(error);
+            return serverError();
+        }
+    }
+}
