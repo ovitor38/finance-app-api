69a5e03fc153b0cee4cbf0364700efec4044c3b4
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-03-05 11:05:50 -0300

    fix(use-case): fixing the right params to update

--- src/use-cases/transactions/update-transaction.js
+++ src/use-cases/transactions/update-transaction.js
@@ -1,20 +1,13 @@
-import { UserNotFoundError } from "../../errors/users";
-
 export class UpadateTransactionUseCase {
-    constructor(getUserByIdRepository, updateTransanctionRepository) {
-        this.getUserByIdRepository = getUserByIdRepository;
+    constructor(updateTransanctionRepository) {
         this.updateTransanctionRepository = updateTransanctionRepository;
     }
 
-    async execute(params) {
-        const user = await this.getUserByIdRepository.execute(params.userId);
-
-        if (!user) {
-            throw new UserNotFoundError();
-        }
-
-        const transaction =
-            await this.updateTransanctionRepository.execute(params);
+    async execute(transactionId, params) {
+        const transaction = await this.updateTransanctionRepository.execute(
+            transactionId,
+            params,
+        );
 
         return transaction;
     }
