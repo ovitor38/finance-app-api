0062f49f2533ad89607bcb8ee996cb0047d17f13
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-03-07 13:49:27 -0300

    fix(use-case): ensure throw error when user not find on update user use case

--- src/use-cases/users/get-user-by-id.js
+++ src/use-cases/users/get-user-by-id.js
@@ -1,9 +1,15 @@
+import { UserNotFoundError } from "../../errors/users";
+
 export class GetUserByIdUseCase {
     constructor(getUserByIdRepository) {
         this.getUserByIdRepository = getUserByIdRepository;
     }
     async execute(userId) {
         const user = await this.getUserByIdRepository.execute(userId);
+
+        if (!user) {
+            throw new UserNotFoundError();
+        }
 
         return user;
     }
