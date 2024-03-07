7333842dd9f4d4daa0613cfb1b1da06d7cb85d8a
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-03-07 13:44:47 -0300

    feat(repository): add get user balance repository

--- src/repositories/postgres/users/index.js
+++ src/repositories/postgres/users/index.js
@@ -3,3 +3,4 @@ export * from "./get-user-by-email.js";
 export * from "./get-user-by-id.js";
 export * from "./update-user.js";
 export * from "./delete-user.js";
+export * from "./get-user-balance.js";
